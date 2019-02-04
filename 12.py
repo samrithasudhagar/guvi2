f6a82001ac14279cf69b2d6333a359776f0a7326
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-02-04 20:26:47 +0530

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,12 @@
+n,k=map(int,input().split())
+p=[]
+s=""
+l=list(map(int,input().split()))
+for i in range(0,k):
+	l.append(l[0])
+	l.remove(l[0])
+for i in l:
+   
+        s=s+str(i)+" "
+print(s)
+	
