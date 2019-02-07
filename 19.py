e7c7d60d7a18b72a50f462e69a044bb021551a48
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-02-07 10:55:32 +0530

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,19 @@
+n=int(input())
+l=[]
+flag=1
+a=[]
+s=""
+for i in range(n):
+    for j in range(n):
+        if i*j==n:
+            l.append(i)
+for i in l:
+    for j in range(2,i):
+        if i%j==0:
+            flag=0
+            break
+        else:
+            flag=1
+    if flag==1:
+        s=s+str(i)+" "
+print(s.strip())
