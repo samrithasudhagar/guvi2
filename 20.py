89ba2dc961abb32d7527bddfb577aadec4b34e82
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-02-07 11:12:12 +0530

    Create 20.py

---
+++ 20.py
@@ -0,0 +1,29 @@
+s=input()
+a=""
+for i in s:
+    if i=="x":
+        k="a"
+        a=a+k
+    elif i=="y":
+        k="b"
+        a=a+k
+    elif i=="z":
+        k="c"
+        a=a+k
+    elif i=="Y":
+        k="B"
+        a=a+k
+    elif i=="Z":
+        k="C"
+        a=a+k
+    elif i=="X":
+        k=="A"
+    else:
+        k=ord(i)+3
+        a=a+str(chr(k))
+	
+print(a)
+		
+
+	
+
