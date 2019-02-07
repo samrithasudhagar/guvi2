2eeb75aed2c20df533183d6aec3f5459d1d9387b
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-02-07 12:43:17 +0530

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,12 @@
+x1,y1=map(int,input().split())
+x2,y2=map(int,input().split())
+x3,y3=map(int,input().split())
+if x1==y1 and x2==y2 and x3==y3:
+	print("yes")
+elif x1==x2 and x1==x3:
+	print("yes")
+elif y1==y2 and y1==y3:
+	print("yes")
+else:
+	print("no")
+	
