926588e8c39cf1f82803cb72dae0456b2db4b7a4
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-02-09 17:13:19 +0530

    Update 35.py

--- 35.py
+++ 35.py
@@ -4,7 +4,7 @@ l=[]
 for i in s:
 	l.append(s.count(i))
 min1=min(l)
-for i in range(0,len(l)):
-	if l[i]==min1:
-		k=k+s[i]
-print(k)
+for i in s:
+	if s.count(i)==min1 and i!=" ":
+		k=k+i.lower()+" "
+print(k.strip())
