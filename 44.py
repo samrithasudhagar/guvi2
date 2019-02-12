c08f6ff44949598fe8a7306e2564bbccd832f206
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-02-12 20:48:34 +0530

    Update 44.py

--- 44.py
+++ 44.py
@@ -1,12 +1,7 @@
 s,n=map(str,input().split())
 n=int(n)
-s=list(s)
 a=""
-for i in range(n):
-    s.append(s[0])
-    s.remove(s[0])
-for i in s:
-    a=a+i
-    
-print(a)
+k=""
+k=s[-(n):]+s[:-(n)]
+print(k)
     
