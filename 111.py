749ae5f9762c022a8773d329d76d0893d69477c0
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-05-22 16:10:15 +0530

    Update 111.py

--- 111.py
+++ 111.py
@@ -1,11 +1,13 @@
 n,k=map(int,input().split())
-l=list(map(int,input().split()))
-a=[]
-for i in range(n):
-    for j in range(n,n+k):
-        if l[i]==l[j]:
-            if l[i] not in a:
-                a.append(l[i])
-a.sort()
-print(*a)
+l=[int(i) for i in input().split()]
+t=l[0:n]
+s=l[n:]
+l=[]
+for i in t:
+	if i in s:
+		l.append(i)
+		s.remove(i)
+	    
+print(*(l))
+
          
