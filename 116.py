58fbe829bb35553de8604f0d1623ec14bcdbd3c3
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-05-22 19:33:35 +0530

    Update 116.py

--- 116.py
+++ 116.py
@@ -7,11 +7,11 @@ a=[]
 j=0
 for i in k:
     c.append(l.count(i))
-while j<len(c)+1:
-    for i in range(len(c)):
-        if max(c)==c[i] and max(c)!=0:
-            a.append(k[i])
-            c[i]=0
-        j=j+1
+while j<len(c):
+    f=c.index(max(c))
+    a.append(k[f])
+    c[f]=0
+    j=j+1
 
 print(*a)
+    
