ff181a9065665f39338c59fa4827b3200d71e518
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-05-24 21:06:44 +0530

    Update 173.py

--- 173.py
+++ 173.py
@@ -4,13 +4,13 @@ p=[]
 if s.isdigit()==False and a.isdigit()==False:
     s=s.split()
     a=a.split()
-for i in s:
-    if i not in a:
+for i in a:
+    if i not in s:
         p.append(i)
     elif s.count(i)!=a.count(i):
         p.append(i)
-for i in a:
-    if i not in s:
+for i in s:
+    if i not in a:
         p.append(i)
     elif a.count(i)!=s.count(i):
         p.append(i)
