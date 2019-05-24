e286eafe77f0e16d8652e6d6b604a1147dd8b296
samrithasudhagar <46440434+samrithasudhagar@users.noreply.github.com>
2019-05-24 22:28:19 +0530

    Create 177.py

---
+++ 177.py
@@ -0,0 +1,10 @@
+s=list(map(str,input().split()))
+a=[]
+for i in s:
+    k=sorted(i)
+    a.append(k)
+for i in range(len(a)):
+    for j in a[i]:
+        print(j,end="")
+    if i!=len(a)-1:
+        print(" ",end="")
